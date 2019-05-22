02a748f13922183c05e3cf3b2cbb0686d3d71810
João Henrique <37977379+mannuscritto@users.noreply.github.com>
2019-05-22 16:37:17 -0300

    Função grausLista criada
    Compila, mas o resultado está errado

--- graf-listadj.h
+++ graf-listadj.h
@@ -10,5 +10,6 @@ typedef struct __noh_adj * LISTAADJ;
 
 int adicionarAresta (LISTAADJ *, int);   // elemento do vetor (x) e destino (y)
 void mostraGrafoLista (LISTAADJ *, int); // vetor de listas e max vertice
+int grausLista(LISTAADJ *, int, int, int *, int *);
 
 #endif
--- main.c
+++ main.c
@@ -4,52 +4,51 @@
 #include "graf-listadj.h"
 #include "graf-matrizadj.h"
 
-int main(int argc, char *argv[])
-{
-  short MATRIZ[VERTICES][VERTICES];
-  LISTAADJ ADJACENCIAS[VERTICES];
-  
-  int numVertices;
-  int x, y;
-  int gS, gE;
-  
-  for (x = 0; x < VERTICES; x++)
-  {
-    ADJACENCIAS[x] = NULL;
-    for (y = 0; y < VERTICES; y++)
-      MATRIZ[x][y] = 0;
-  }
-  
-  numVertices = 0;
-  
-  while (1)
-  {
-     printf("Digite a origem e o destino da aresta: ");
-     scanf("%d %d", &x, &y);
-     if ((x == -1) || (y == -1))
-        break;
-     if ((x >= 0) && (x < VERTICES) && (y >= 0) && (y < VERTICES))
-     {
-        MATRIZ[x][y] = 1;
-        adicionarAresta (&ADJACENCIAS[x], y);
-        if (x > numVertices)
-           numVertices = x;
-        if (y > numVertices)
-           numVertices = y;
-     }
-  }
-  numVertices = numVertices + 1;
-  
-  mostraGrafo (MATRIZ, numVertices);
-  mostraGrafoLista (ADJACENCIAS, numVertices);
-  
-  printf(" V  gE  gS\n");
-  for (x = 0; x < numVertices; x++)
-  {
-     graus(MATRIZ, x, &gE, &gS);
-     printf("%2d %3d %3d\n", x, gE, gS);
-  }
-  
-  system("PAUSE");	
-  return 0;
+int main(int argc, char *argv[]) {
+	short MATRIZ[VERTICES][VERTICES];
+	LISTAADJ ADJACENCIAS[VERTICES];
+	
+	int numVertices = 0;
+	int x, y;
+	int gS, gE;
+	
+	for (x = 0; x < VERTICES; x++) {
+		ADJACENCIAS[x] = NULL;
+		for (y = 0; y < VERTICES; y++)
+			MATRIZ[x][y] = 0;
+	}
+	
+	while (1) {
+		printf("Digite a origem e o destino da aresta: ");
+		scanf("%d %d", &x, &y);
+		if ((x == -1) || (y == -1))
+			break;
+		if ((x >= 0) && (x < VERTICES) && (y >= 0) && (y < VERTICES)) {
+			MATRIZ[x][y] = 1;
+			adicionarAresta (&ADJACENCIAS[x], y);
+			if (x > numVertices)
+				numVertices = x;
+			if (y > numVertices)
+				numVertices = y;
+		}
+	}
+	numVertices = numVertices + 1;
+	
+	mostraGrafo (MATRIZ, numVertices);
+	mostraGrafoLista (ADJACENCIAS, numVertices);
+	
+	printf(" V  gE  gS\n");
+	for (x = 0; x < numVertices; x++) {
+		graus(MATRIZ, x, &gE, &gS);
+		printf("%2d %3d %3d\n", x, gE, gS);
+	}
+	
+	printf(" V  gE  gS\n");
+	for (x = 0; x < numVertices; x++) {
+		grausLista(ADJACENCIAS, numVertices, x, &gE, &gS);
+		printf("%2d %3d %3d\n", x, gE, gS);
+	}
+	
+	system("PAUSE");	
+	return 0;
 }
